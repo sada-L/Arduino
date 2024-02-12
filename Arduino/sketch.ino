e6be7cbc9fe156093f64fd682697af729f40d63d
Sada <114806441+sada-L@users.noreply.github.com>
2024-02-12 16:04:42 +0300

    Add files via upload

---
+++ Arduino/sketch.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(13, HIGH);
+    digitalWrite(13, HIGH);
+    delay(3000);
+    digitalWrite(13, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(1000);
+}
